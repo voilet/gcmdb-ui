277dfb31cc11c3338781eec15938ad284c0498b2
a0x55aa <admin@0x55aa.com>
2018-02-01 14:09:46 +0800

    登录跳转

--- src/models/login.js
+++ src/models/login.js
@@ -48,8 +48,7 @@ export default {
 
   reducers: {
     changeLoginStatus(state, { payload }) {
-      // setAuthority(payload.currentAuthority);
-      setAuthority('admin');
+      setAuthority(payload.currentAuthority);
       return {
         ...state,
         status: payload.status,
